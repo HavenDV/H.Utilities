f286b81d2f4c2f0dc17ddd9b34318fa46f7efeff
Konstantin S <HavenDV@users.noreply.github.com>
2021-01-18 01:22:14 +1000

    test: Added new tests.

--- src/tests/H.Utilities.Screenshoter.IntegrationTests/SimpleTests.cs
+++ src/tests/H.Utilities.Screenshoter.IntegrationTests/SimpleTests.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace H.Utilities.IntegrationTests
@@ -20,6 +24,34 @@ namespace H.Utilities.IntegrationTests
             {
                 UseShellExecute = true,
             });
+        }
+
+        [TestMethod]
+        public async Task AsyncShotTest()
+        {
+            using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+            var cancellationToken = cancellationTokenSource.Token;
+
+            var image = await Screenshoter.ShotAsync(cancellationToken: cancellationToken);
+            var path = $"{Path.GetTempFileName()}.bmp";
+
+            image.Save(path);
+
+            Process.Start(new ProcessStartInfo(path)
+            {
+                UseShellExecute = true,
+            });
+        }
+
+        [TestMethod]
+        public async Task AsyncMultiShotTest()
+        {
+            using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+            var cancellationToken = cancellationTokenSource.Token;
+
+            await Task.WhenAll(Enumerable
+                .Range(0, 10)
+                .Select(_ => Screenshoter.ShotAsync(cancellationToken: cancellationToken)));
         }
 
         [TestMethod]
